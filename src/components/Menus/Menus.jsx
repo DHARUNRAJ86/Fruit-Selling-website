6d767c87ff0a27baf3f1e5d4323ef0b5f52ccb95
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-02-22 09:39:39 +0530

    updated code

--- src/components/Menus/Menus.jsx
+++ src/components/Menus/Menus.jsx
@@ -3,24 +3,33 @@ import Fruit1 from "../../assets/fruits/apple.png"
 import Fruit2 from "../../assets/fruits/orange.png"
 import Fruit3 from "../../assets/fruits/avocado.png"
 import Fruit4 from "../../assets/fruits/cherry.png"
+import {AnimatePresence,motion} from 'framer-motion'
+import {FadeLeft} from '../../utility/animation'
 
 const MenusData = [
-    { id: 1, title: "Fresh Red Apples", price: "₹60", img: Fruit1 },
-    { id: 2, title: "Fresh Oranges", price: "₹80", img: Fruit2 },
-    { id: 3, title: "Fresh Avocados", price: "₹120", img: Fruit3 },
-    { id: 4, title: "Fresh Cherries", price: "₹200", img: Fruit4 }
+    { id: 1, title: "Fresh Red Apples", price: "₹60", img: Fruit1,delay:0.3},
+    { id: 2, title: "Fresh Oranges", price: "₹80", img: Fruit2,delay:0.4},
+    { id: 3, title: "Fresh Avocados", price: "₹120", img: Fruit3,delay:0.5 },
+    { id: 4, title: "Fresh Cherries", price: "₹200", img: Fruit4,delay:0.6}
 ];
 
 const Menus = () => {
     return (
         <section>
             <div className="container mx-auto px-4 pt-12 pb-20">
-                <h1 className='text-2xl font-bold text-left pb-16 uppercase'>Our Menu</h1>
+                <motion.h1
+                 initial={{opacity:0,x:-200}}
+                 whileInView={{opacity:1,x:0}}
+                 transition={{duration:1,delay:0.3}}
+                 className='text-2xl font-bold text-left pb-16 uppercase'>Our Menu</motion.h1>
                 
                 {/* Grid container with more gap to allow for the pop-out fruit */}
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10">
                     {MenusData.map((menu) => (
-                        <div 
+                        <motion.div 
+                            variants={FadeLeft(menu.delay)}
+                            initial="hidden"
+                            whileInView="visible"
                             key={menu.id} 
                             className='bg-white rounded-2xl  py-2 shadow-[0_0_22px_0_rgba(0,0,0,0.15)] flex flex-row items-center justify-between gap-3 group hover:scale-105 duration-300 transition-all'
                         >
@@ -41,7 +50,7 @@ const Menus = () => {
                                     {menu.price}
                                 </p>
                             </div>
-                        </div>
+                        </motion.div>
                     ))}
                 </div>
             </div>

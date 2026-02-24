1cc94c2b1d041933519780d849a80acac1a92b0c
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-02-24 18:49:51 +0530

    updated code

--- src/components/Banners/Banner.jsx
+++ src/components/Banners/Banner.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import BannerPng from '../../assets/fruits/fruits-splash.png'
+import {motion} from 'framer-motion'
+import {FadeUp} from '../../utility/animation'
 
 const Banner =()=>{
     return(
         <section>
             <div className="container">
                 {/*Banner Image */}
+                <div>
+                    <img src={BannerPng} alt="" className='w-[250px] md:max-w-[400px] h-full object-cover'/>
+                </div>
                 {/*Brand Info */}
+                <div>
+                    <div>
+                        <h1>Brand Info</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium nobis aliquam veritatis autem excepturi hic aut iusto minus tempora et, in eaque aspernatur eveniet officiis provident! Repellat quaerat ipsam quasi</p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis veniam delectus vel harum, excepturi maiores natus facere minima eum cum.</p>
+                    </div>
+                </div>
             </div>
         </section>
     )

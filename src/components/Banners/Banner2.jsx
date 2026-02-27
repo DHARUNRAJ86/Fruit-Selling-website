0b2d71dda4702b5608e7c4b3b512aadd19072f9a
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-02-27 19:46:22 +0530

    updated code

--- src/components/Banners/Banner2.jsx
+++ src/components/Banners/Banner2.jsx
@@ -1,21 +1,13 @@
 import React from 'react'
-import BannerPng from '../../assets/fruits/fruits-splash.png'
+import BannerPng from '../../assets/fruits/fruit-plate2.png'
 import {motion} from 'framer-motion'
 import {FadeUp,FadeLeft} from '../../utility/animation'
 
 const Banner =()=>{
     return(
-        <section className='bg-orange-400/10'>
+        <section className='ml-25 mt-10 mb-10'>
             <div className="container grid grid-cols-1 md:grid-cols-2 space-y-6 md:space-y-0 ">
-                {/*Banner Image */}
-                <div className='flex justify-center items-center'>
-                    <motion.img
-                       initial={{opacity:0,scale:0.5}}
-                       whileInView={{opacity:1,scale:1}}
-                       transition={{type:"spring",stiffness:100,delay:0.5}}
-                       viewport={{once:true}}
-                     src={BannerPng} alt="" className='w-[260px] md:max-w-[400px] h-full object-cover'/>
-                </div>
+               
                 {/*Brand Info */}
                 <div className='flex flex-col justify-center'>
                     <div className='text-center md:text-left space-y-4'>
@@ -51,10 +43,17 @@ const Banner =()=>{
                                     </motion.div>
                     </div>
                 </div>
+                 {/*Banner Image */}
+                <div className='flex justify-center items-center'>
+                    <motion.img
+                       initial={{opacity:0,scale:0.5}}
+                       whileInView={{opacity:1,scale:1}}
+                       transition={{type:"spring",stiffness:100,delay:0.5}}
+                       viewport={{once:true}}
+                     src={BannerPng} alt="" className='w-[260px] md:max-w-[400px] h-full object-cover'/>
+                </div>
             </div>
-            <br />
         </section>
-        
     )
 }
 export default Banner
